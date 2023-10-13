b25299657dc688d18c06537d186ff2b8b8ae7665
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2023-10-13 13:40:47 +0100

    DR-468 Reconfigured after comments

--- scripts/sql/aurora_extract_scripts/sample-healthcareservice-json.sql
+++ scripts/sql/aurora_extract_scripts/sample-healthcareservice-json.sql
@@ -1,80 +1,61 @@
 select
-json_build_object(
-  'services',
-  json_agg(
     json_build_object(
-      'resourceType','HealthcareService',
-      'identifier', identifier.identifier,
-      'active','true',
-      'providedBy',
-      json_build_object(
-        'resourceType', 'Organization',
-        'identifier', 'TBC' ),
-      'type',
-      st.name,
-      'location',
-      json_build_object(
-        'resourceType', 'Location',
-        'identifier', 'TBC' ),
-      'service_name',
-      (case
-        when length(s.odscode) = 5 then 'General Pharmacy Service'
-        when s.name like 'DSP%' then 'Distance Selling Service'
-        when s.name like 'CPCS%' then 'Community Pharmacy Service'
-        when s.name like 'Pharm+%' then 'Community Pharmacy Service'
-        when s.name = s.publicname then split_part(s.name,'-',1)
-        else s.name end) ,
-      'telecom', telecom.telecoms,
-      'serviceProfiles',
-      serviceProfiles
-    )
-  )
-) services
-from pathwaysdos.services s inner join pathwaysdos.servicetypes st on s.typeid=st.id
-left join (
-  select
-  id,
-    json_agg(
-      json_build_object(
-        'use', 'old',
-        substr(c.identifier,1,position('-' IN c.identifier)-1) ,
-        split_part(c.identifier,'-',2)
-      )
-    ) identifier
-from pathwaysdos.services  s
-cross join lateral (values ('uid-'||uid),('serviceid-'||id)) as c(identifier)
-group by id
-) identifier
-on s.id = identifier.id
-left join (
-  select
-  id,
-    json_agg(
-      json_build_object(
-        substr(c.telecom,1,position('-' IN c.telecom)-1),
-        split_part(c.telecom,'-',2)
-      )
-    ) telecoms
-  from pathwaysdos.services  s
-  cross join lateral (values ('publicphone-'||publicphone),('nonpublicphone-'||nonpublicphone),('fax-'||fax)) as c(telecom)
-  where  c.telecom is not null
-  group by id
-) telecom
-on s.id = telecom.id
-left join (
-  select
-  serviceid,
-  'To be defined by the DoS lead' as name,
-  json_agg(
-    json_build_array(rr.name)
-    ) eligibleFor
-  from pathwaysdos.servicereferralroles sr
-  inner join pathwaysdos.referralroles rr
-  on sr.referralroleid=rr.id group by serviceid
+        'services', json_agg(
+            json_build_object(
+                'resourceType','HealthcareService',
+                'identifier',
+                json_build_array(
+                json_build_object('use','official','type','NEWID','value',md5(s.id::text)),
+                json_build_object('use','old','type','DOSID','value',s.id),
+                json_build_object('use','old','type','UID','value', s.uid)
+                ),
+              'active','true',
+              'providedBy',
+                json_build_object(
+                  'resourceType', 'Organization',
+                  'identifier', 'TBC'
+                ),
+              'type', 'Pharmacy',
+              'location',
+              json_build_object(
+                  'resourceType', 'Location',
+                  'identifier', 'TBC'
+              ),
+              'service_name',
+              (case
+              when length(s.odscode) = 5 then 'General Pharmacy Service'
+              when s.name like 'DSP%' then 'Distance Selling Service'
+              when s.name like 'CPCS%' then 'Community Pharmacy Service'
+              when s.name like 'Pharm+%' then 'Community Pharmacy Service'
+              when s.name = s.publicname then split_part(s.name,'-',1)
+              else s.name end) ,
+              'telecom',
+              json_build_array(
+                json_build_object('system','phone','value',s.publicphone, 'use', 'public'),
+                json_build_object('system','fax','value',s.fax, 'use', 'non-public'),
+                json_build_object('system','email','value',s.email, 'use', 'non-public')
+              ),
+              'serviceProfiles',serviceProfiles
+            )
+        )
+    ) services
+from pathwaysdos.services s
+left join
+(select
+    serviceid,
+    'To be defined by the DoS lead' as name
+    ,json_agg(
+        json_build_array(rr.name)) eligibleFor
+        from pathwaysdos.servicereferralroles sr
+        inner join pathwaysdos.referralroles rr
+        on sr.referralroleid=rr.id
+        group by serviceid
 ) serviceProfiles
 on s.id = serviceProfiles.serviceid
+
 where s.id in (169621,
 143973,
 127423,
 107275,
-107273);
+107273)
+;
