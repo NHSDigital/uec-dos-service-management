14b87f43546088bfeb9ba2699805b358940a3217
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2023-10-13 09:09:50 +0100

    DR-468 Formatting changes to space

--- scripts/sql/aurora_extract_scripts/sample-healthcareservice-json.sql
+++ scripts/sql/aurora_extract_scripts/sample-healthcareservice-json.sql
@@ -11,50 +11,54 @@ json_build_object(
         'resourceType', 'Organization',
         'identifier', 'TBC' ),
       'type',
-st.name,
-'location',
-json_build_object(
-'resourceType', 'Location',
-'identifier', 'TBC' ),
-'service_name',
-(case
-when length(s.odscode) = 5 then 'General Pharmacy Service'
-when s.name like 'DSP%' then 'Distance Selling Service'
-when s.name like 'CPCS%' then 'Community Pharmacy Service'
-when s.name like 'Pharm+%' then 'Community Pharmacy Service'
-when s.name = s.publicname then split_part(s.name,'-',1)
-else s.name end) ,
-'telecom', telecom.telecoms,
-'serviceProfiles',
-serviceProfiles
-)
-)
+      st.name,
+      'location',
+      json_build_object(
+        'resourceType', 'Location',
+        'identifier', 'TBC' ),
+      'service_name',
+      (case
+        when length(s.odscode) = 5 then 'General Pharmacy Service'
+        when s.name like 'DSP%' then 'Distance Selling Service'
+        when s.name like 'CPCS%' then 'Community Pharmacy Service'
+        when s.name like 'Pharm+%' then 'Community Pharmacy Service'
+        when s.name = s.publicname then split_part(s.name,'-',1)
+        else s.name end) ,
+      'telecom', telecom.telecoms,
+      'serviceProfiles',
+      serviceProfiles
+    )
+  )
 ) services
 from pathwaysdos.services s inner join pathwaysdos.servicetypes st on s.typeid=st.id
 left join (
-    select
-        id,
-        json_agg(
-            json_build_object(
-'use', 'old',
-substr(c.identifier,1,position('-' IN c.identifier)-1) ,
-split_part(c.identifier,'-',2) )) identifier
+  select
+  id,
+    json_agg(
+      json_build_object(
+        'use', 'old',
+        substr(c.identifier,1,position('-' IN c.identifier)-1) ,
+        split_part(c.identifier,'-',2)
+      )
+    ) identifier
 from pathwaysdos.services  s
 cross join lateral (values ('uid-'||uid),('serviceid-'||id)) as c(identifier)
 group by id
 ) identifier
 on s.id = identifier.id
 left join (
-select
-id,
-json_agg(
-json_build_object(
-substr(c.telecom,1,position('-' IN c.telecom)-1),
-split_part(c.telecom,'-',2))) telecoms
-from pathwaysdos.services  s
-cross join lateral (values ('publicphone-'||publicphone),('nonpublicphone-'||nonpublicphone),('fax-'||fax)) as c(telecom)
-where  c.telecom is not null
-group by id
+  select
+  id,
+    json_agg(
+      json_build_object(
+        substr(c.telecom,1,position('-' IN c.telecom)-1),
+        split_part(c.telecom,'-',2)
+      )
+    ) telecoms
+  from pathwaysdos.services  s
+  cross join lateral (values ('publicphone-'||publicphone),('nonpublicphone-'||nonpublicphone),('fax-'||fax)) as c(telecom)
+  where  c.telecom is not null
+  group by id
 ) telecom
 on s.id = telecom.id
 left join
