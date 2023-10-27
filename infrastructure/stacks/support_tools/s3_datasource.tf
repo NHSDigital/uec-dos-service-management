840068153925d5a2e5de8ee86c3da2c0888c2cbd
Jonathan Pearce <j.pearce1@nhs.net>
2023-10-27 12:30:51 +0100

    DR-475 Ods location comparator

--- infrastructure/stacks/support_tools/s3_datasource.tf
+++ infrastructure/stacks/support_tools/s3_datasource.tf
@@ -1,4 +1,4 @@
 module "sm_datasource_bucket" {
   source      = "../../modules/s3"
-  bucket_name = var.sm_datasource_bucket_name
+  bucket_name = "${var.sm_datasource_bucket_name}${local.workspace_suffix}"
 }
