2db1bfbb7d3c19b38bb9741fd3ebd2e387b7a9ce
Tim Rickwood <tim.rickwood@nhs.net>
2023-11-16 13:26:15 +0000

    DR-419 Add workspace to github output

--- scripts/functions/git-functions.sh
+++ scripts/functions/git-functions.sh
@@ -80,6 +80,7 @@ function export_terraform_workspace_name {
         fi
     else
       BRANCH_NAME="${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
+      BRANCH_NAME=$(echo $BRANCH_NAME | sed 's/refs\/heads\/task/task/g')
       if [ "$BRANCH_NAME" != 'main' ] && [[ $BRANCH_NAME =~ $GIT_BRANCH_PATTERN ]]  ; then
         IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
         IFS='_' read -r -a ref <<< "${name_array[1]}"
