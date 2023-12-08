e08d76abd1b874805ab54f29c6c6ffae1aa39c11
Tim Rickwood <tim.rickwood@nhs.net>
2023-12-08 12:53:14 +0000

    DR-559 Refactor unit tests

--- scripts/twr.sh
+++ scripts/twr.sh
@@ -6,7 +6,6 @@ set -e
 #
 APPLICATION_DIR=application
 APPLICATION_UTIL_DIR=application-utils
-# APPLICATION_TEST_DIR='tests/unit'
 
 echo "Precautionary removal of temporary files"
 for path in "$APPLICATION_DIR"/*/ ; do
@@ -30,8 +29,8 @@ for path in "$APPLICATION_DIR"/*/ ; do
             if ! [ "$dir" == $APPLICATION_DIR ] ; then
                 if [ -d $APPLICATION_DIR/"$dir/test" ]; then
                     echo "Preparing tests for $dir"
-                    cp -r $APPLICATION_DIR/"$dir"/test/test_* $APPLICATION_DIR/"$dir"
-                    cp -r $APPLICATION_UTIL_DIR/* $APPLICATION_DIR/"$dir"
+                    # copy util code but not the test code
+                    rsync -av --exclude='test/' $APPLICATION_UTIL_DIR/* $APPLICATION_DIR/"$dir"
                     echo "Running tests for $dir"
                     pip install -r $APPLICATION_DIR/"$dir"/test/requirements.txt
                     coverage run -a --source=$APPLICATION_DIR/"$dir"  -m pytest $APPLICATION_DIR/"$dir"
@@ -42,29 +41,28 @@ for path in "$APPLICATION_DIR"/*/ ; do
         done
 done
 
-# # test utils
-# for path in "$APPLICATION_UTIL_DIR"/*/ ; do
-#     dirs=$(echo "$path" | tr "\/" '\n')
-#     for dir in $dirs
-#         do
-#             if ! [ "$dir" == $APPLICATION_UTIL_DIR ] ; then
-#                 if [ -d $APPLICATION_UTIL_DIR/"$dir/test" ]; then
-#                     echo "Preparing tests for $dir"
-#                     cp -r $APPLICATION_UTIL_DIR/"$dir"/test/test_* $APPLICATION_UTIL_DIR/"$dir"
-#                     # cp -r $APPLICATION_UTIL_DIR/* $APPLICATION_DIR/"$dir"
-#                     echo "Running tests for $dir"
-#                     # pip install -r $APPLICATION_DIR/"$dir"/test/requirements.txt
-#                     coverage run -a --source=$APPLICATION_UTIL_DIR/"$dir"  -m pytest $APPLICATION_UTIL_DIR/"$dir"
-#                 else
-#                     echo "No tests written for $dir"
-#                 fi
-#             fi
-#         done
-# done
+# test utils
+for path in "$APPLICATION_UTIL_DIR"/*/ ; do
+    dirs=$(echo "$path" | tr "\/" '\n')
+    for dir in $dirs
+        do
+            if ! [ "$dir" == $APPLICATION_UTIL_DIR ] ; then
+                if [ -d $APPLICATION_UTIL_DIR/"$dir/test" ]; then
+                    echo "Preparing tests for $dir"
+                    echo "Running tests for $dir"
+                    pip install -r $APPLICATION_UTIL_DIR/"$dir"/test/requirements.txt
+                    coverage run -a --source=$APPLICATION_UTIL_DIR/"$dir" -m pytest $APPLICATION_UTIL_DIR/"$dir"
+                else
+                    echo "No tests written for $dir"
+                fi
+            fi
+        done
+done
 
 #  use requirements defined for tests/unit
-coverage report
-coverage html
+coverage report -i
+coverage html -i
+coverage xml -i
 
 # now clear down copied test code after testing
 echo "Removing temporary files"
