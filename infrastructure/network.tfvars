cb01529099fc04fcf2a67e5bf06cd6285d0d2ea1
Tim Rickwood <tim.rickwood@nhs.net>
2023-12-14 10:11:24 +0000

    DR-319 Add data look up for certs

--- infrastructure/network.tfvars
+++ infrastructure/network.tfvars
@@ -1,1 +1,3 @@
-vpc_name = "sm-vpc"
+vpc_name                    = "sm-vpc"
+log_group_retention_in_days = 365
+recovery_window_in_days     = 0
--- infrastructure/stacks/network/vpn.tf
+++ infrastructure/stacks/network/vpn.tf
@@ -1,29 +1,25 @@
-# three routes one per subnet
-resource "aws_ec2_client_vpn_route" "example" {
+# three routes - one per subnet
+resource "aws_ec2_client_vpn_route" "client_route" {
   count                  = length(data.aws_availability_zones.azs.names)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.service_management_vpn.id
+  description            = "Client route for internet access to service management vpn"
   destination_cidr_block = "0.0.0.0/0"
   target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.network_assoc_service_management[count.index].subnet_id
 }
-# three associations one per subnet
+# three associations - one per subnet
 resource "aws_ec2_client_vpn_network_association" "network_assoc_service_management" {
   count                  = length(data.aws_availability_zones.azs.names)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.service_management_vpn.id
   subnet_id              = resource.aws_subnet.private_zone[count.index].id
 }
-# three authorization rules one per subnet
+# three authorization rules - one per subnet
 resource "aws_ec2_client_vpn_authorization_rule" "auth_rule_service_management_vpn" {
   count                  = length(data.aws_availability_zones.azs.names)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.service_management_vpn.id
   target_network_cidr    = resource.aws_subnet.private_zone[count.index].cidr_block
   authorize_all_groups   = true
 }
-# resource "aws_ec2_client_vpn_authorization_rule" "authorization_rule" {
-#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.my_client_vpn.id
 
-#   target_network_cidr    = "10.0.0.0/16"
-#   authorize_all_groups   = true
-# }
 
 # one security group
 resource "aws_security_group" "vpn_secgroup" {
@@ -59,12 +55,12 @@ resource "aws_ec2_client_vpn_endpoint" "service_management_vpn" {
 
   security_group_ids     = [aws_security_group.vpn_secgroup.id]
   split_tunnel           = true
-  server_certificate_arn = aws_acm_certificate.server_vpn_cert.arn
-  client_cidr_block      = "10.0.0.0/16"
+  server_certificate_arn = data.aws_acm_certificate.vpn_sm_server_cert.arn
+  client_cidr_block      = "10.0.0.0/22"
 
   authentication_options {
     type                       = "certificate-authentication"
-    root_certificate_chain_arn = aws_acm_certificate.client_vpn_cert.arn
+    root_certificate_chain_arn = data.aws_acm_certificate.vpn_sm_server_cert.arn
   }
 
   connection_log_options {
@@ -73,29 +69,42 @@ resource "aws_ec2_client_vpn_endpoint" "service_management_vpn" {
     cloudwatch_log_stream = aws_cloudwatch_log_stream.sm_log_stream.name
   }
   # stuff that has to exist first
-  depends_on = [
-    aws_acm_certificate.server_vpn_cert,
-    aws_acm_certificate.client_vpn_cert
-  ]
+  # depends_on = [
+  #   aws_acm_certificate.server_vpn_cert,
+  #   aws_acm_certificate.client_vpn_cert
+  # ]
 }
 
 resource "aws_cloudwatch_log_group" "sm_log_group" {
-  name = "sm_vpn_log_group"
+  name              = "sm-vpn-log-group"
+  retention_in_days = var.log_group_retention_in_days
 }
 
 resource "aws_cloudwatch_log_stream" "sm_log_stream" {
-  name           = "sm_vpn_log_stream"
+  name           = "sm-vpn-log-stream"
   log_group_name = aws_cloudwatch_log_group.sm_log_group.name
 }
 
-resource "aws_acm_certificate" "server_vpn_cert" {
-  certificate_body  = "TBA"
-  private_key       = "TBA"
-  certificate_chain = "TBA"
+# resource "aws_acm_certificate" "server_vpn_cert" {
+#   certificate_body  = "TBA"
+#   private_key       = "TBA"
+#   certificate_chain = "TBA"
+# }
+
+# resource "aws_acm_certificate" "client_vpn_cert" {
+#   certificate_body  = "TBA"
+#   private_key       = "TBA"
+#   certificate_chain = "TBA"
+# }
+
+# secrets manager to hold ca cert
+resource "aws_secretsmanager_secret" "cert_secret" {
+  name = "${var.repo_name}/vpn_cert"
+  recovery_window_in_days = var.recovery_window_in_days
 }
 
-resource "aws_acm_certificate" "client_vpn_cert" {
-  certificate_body  = "TBA"
-  private_key       = "TBA"
-  certificate_chain = "TBA"
+# secrets manager to hold private key
+resource "aws_secretsmanager_secret" "pk_secret" {
+  name = "${var.repo_name}/vpn_pk"
+  recovery_window_in_days = var.recovery_window_in_days
 }
