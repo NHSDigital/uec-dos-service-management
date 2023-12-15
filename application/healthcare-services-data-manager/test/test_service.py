191e1cdbf3ee86b92b42d38f62981494e236ec67
Tim Rickwood <tim.rickwood@nhs.net>
2023-12-15 10:25:12 +0000

    DR-559 Post rebase changes

--- application/healthcare-services-data-manager/test/test_service.py
+++ application/healthcare-services-data-manager/test/test_service.py
@@ -87,7 +87,7 @@ def test_update_record():
     assert response["Item"]["HospitalName"] == mock_hospital_name
     assert response["Item"]["HospitalLocation"] == mock_hospital_location
     response = service.update_record(
-        mock_id, mock_revised_hospital_location, mock_revised_hospital_name
+        mock_id, mock_revised_hospital_name, mock_revised_hospital_location
     )
     assert response["Attributes"]["HospitalName"] == mock_revised_hospital_name
     assert response["Attributes"]["HospitalLocation"] == mock_revised_hospital_location
