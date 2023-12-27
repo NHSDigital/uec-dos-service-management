dc6c78d0195b6d8bf265e46ef0f1617c51ba86fa
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2023-12-27 10:22:21 +0000

    DR-999 Refactoring to cope with default

--- tests/integration/steps/data_steps.py
+++ tests/integration/steps/data_steps.py
@@ -4,9 +4,12 @@ from utilities import dynamodb
 from utilities.config_reader import read_config
 
 
+
 @given("I reset the data by deleting id {id} in the dynamoDB table {resource_name}")
 def dynamodb_delete(context, id, resource_name):
-    table_name = resource_name + "-" + context.workspace
+    context.resource_name = resource_name
+    # table_name = context.resource_name + "-" + context.workspace
+    table_name = set_tablename(context)
     dynamodb.delete_record_by_id(table_name, id)
 
 
@@ -15,22 +18,33 @@ def dynamodb_delete(context, id, resource_name):
 )
 def dynamodb_add(context, file_name, resource_name):
     body = read_config("json_schema", file_name)
-    table_name = resource_name + "-" + context.workspace
+    context.resource_name = resource_name
+    table_name = set_tablename(context)
+    # table_name = resource_name + "-" + context.workspace
     dynamodb.add_record_from_file(table_name, body)
 
 
 @then("I can retrieve data for id {id} in the dynamoDB table")
 def dynamodb_get(context, id):
-    table_name = context.resource_name + "-" + context.workspace
+    # table_name = context.resource_name + "-" + context.workspace
+    table_name = set_tablename(context)
     response = dynamodb.get_record_by_id(table_name, id)
     assert_that(response["Item"]["id"]).is_equal_to(id)
 
 
-@then("data for id {id} in the dynamoDB table has been deleted")
+@then("the data for id {id} in the dynamoDB table has been deleted")
 def dynamodb_check_delete(context, id):
-    table_name = context.resource_name + "-" + context.workspace
+    # table_name = context.resource_name + "-" + context.workspace
+    table_name = set_tablename(context)
     response = dynamodb.get_record_by_id(table_name, id)
     assert_that(
         response["ResponseMetadata"]["HTTPHeaders"]["content-length"]
     ).is_equal_to("2")
     assert_that(response).does_not_contain("Item")
+
+
+def set_tablename(context):
+    table_name = context.resource_name
+    if context.workspace != "default":
+        table_name = context.resource_name + "-" + context.workspace
+    return table_name
