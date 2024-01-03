f47842f0afdfb978933e6cc511dfac9de3de61e6
Tim Rickwood <tim.rickwood@nhs.net>
2024-01-03 12:12:23 +0000

    DR-595 Fix app deploy from main bug

--- scripts/app-build-deploy.sh
+++ scripts/app-build-deploy.sh
@@ -3,6 +3,8 @@
 #THIS SCRIPT EXECUTES THE RELEVANT DEPLOYMENT ACTIONS FOR THIS MICROSERVICE.
 #IT IS REFERENCED BY THE APPLICATION PIPELINE AND THEREFORE MUST ALWAYS BE CALLED "deploy_service.sh"
 
+# fail on first error
+set -e
 #INPUT ARGUMENTS
 source ./scripts/functions/git-functions.sh
 
@@ -11,9 +13,13 @@ echo "Service Name: ${SERVICE_NAME}"
 echo "Deployment workspace ${DEPLOYMENT_WORKSPACE}"
 export_terraform_workspace_name
 
-SERVICE_NAME_WORKPACE="${SERVICE_NAME}-${TERRAFORM_WORKSPACE_NAME}"
-echo "Service Name Workspace: ${SERVICE_NAME_WORKPACE}"
+FULL_SERVICE_NAME="${SERVICE_NAME}"
 
+if [ "${TERRAFORM_WORKSPACE_NAME}" != "default" ]; then
+  FULL_SERVICE_NAME="${FULL_SERVICE_NAME}-${TERRAFORM_WORKSPACE_NAME}"
+fi
+
+echo "Service Name Workspace: ${FULL_SERVICE_NAME}"
 echo "Copy utility code"
 # copy util code but not the test code
 rsync -av --exclude='test/' ./application-utils/* ./application/"${SERVICE_NAME}"
@@ -22,9 +28,9 @@ cd ./application/"${SERVICE_NAME}"
 
 zip -r deployment.zip .
 
-LAMBDA_OUTPUT=$(aws lambda update-function-code --function-name=$SERVICE_NAME_WORKPACE --zip-file=fileb://deployment.zip --publish)
+LAMBDA_OUTPUT=$(aws lambda update-function-code --function-name="$FULL_SERVICE_NAME" --zip-file=fileb://deployment.zip --publish)
 LATEST_VERSION=$(jq -r '.Version' --compact-output <<< "$LAMBDA_OUTPUT" )
-PREVIOUS_VERSION=$(expr $LATEST_VERSION - 1)
+PREVIOUS_VERSION=$(expr "$LATEST_VERSION" - 1)
 echo "Latest version: ${LATEST_VERSION}"
 echo "Previous version: ${PREVIOUS_VERSION}"
 echo "Tidy up temporary files"
