c7602b7af6e66c795209300740ee67ef33782943
Tamara Goldschmidt <77108863+tamaragoldschmidt@users.noreply.github.com>
2024-01-10 13:49:44 +0000

    Task/DR-565 Organisation CRUD lambda (#51)
    * DR-565 App and service update
    
    * DR-565 App and service update
    
    * DR-565 Add tests for org
    
    * DR-5651 Setting up of int tests
    
    * DR-5651 Organisations not org affiliation
    
    * DR-5651 Removed unrequired tag
    
    ---------
    
    Co-authored-by: pira Maheswaran <piravinth.maheswaran1@nhs.net>

--- application/organisations-data-manager/app.py
+++ application/organisations-data-manager/app.py
@@ -14,41 +14,28 @@ def lambda_handler(event: dict, context: LambdaContext) -> dict:
 @app.post("/organisations")
 def create_organisations():
     post_data: dict = app.current_event.json_body
-
-    data = {
-        "id": post_data["id"],
-        "HospitalName": post_data["HospitalName"],
-        "HospitalLocation": post_data["HospitalLocation"],
-    }
-
-    print(data)
-    service.add_record(data)
-
-    return {"statusCode": 200, "body": "Item Added Successfully"}
+    response = service.add_record(post_data)
+    return response
 
 
 # Get using query string approach
 @app.get("/organisations")
 def get_organisations():
-    hs_id = app.current_event.get_query_string_value(name="id", default_value="")
-    print("Get hs_id record..." + hs_id)
-    response = service.get_record_by_id(hs_id)
-    return {"statusCode": 200, "body": response}
+    o_id = app.current_event.get_query_string_value(name="id", default_value="")
+    response = service.get_record_by_id(o_id)
+    return response
 
 
 @app.put("/organisations")
 def update_organisations():
     put_data: dict = app.current_event.json_body
-    service.update_record(
-        put_data["id"], put_data["HospitalName"], put_data["HospitalLocation"]
-    )
-    return {"statusCode": 200, "body": "Item Updated Successfully"}
+    response = service.update_record(put_data)
+    return response
 
 
 @app.delete("/organisations")
 def delete_organisations():
     delete_data: dict = app.current_event.json_body
-    print("Delete organisationaffiliations record...")
     o_id = delete_data["id"]
-    service.delete_record(o_id)
-    return {"statusCode": 200, "body": "Item Deleted Successfully"}
+    response = service.delete_record(o_id)
+    return response
