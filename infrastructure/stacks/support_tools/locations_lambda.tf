4aaae04e03f17736d267c6b4f48ca8b46782b5c3
mindaugas-nhsdos <150432888+mindaugas-nhsdos@users.noreply.github.com>
2024-01-19 10:03:33 +0000

    DR-537 Location data lambda (#48)
    * DR-537 Location data lambda
    
    * DR-537 Removed dup var and tf format
    
    * DR-537 Changed zip source to dir
    
    * DR-537 Fixing failed checks
    
    * DR-537 Fixing tf format
    
    * DR-537 Removing variable description
    
    * DR-537 Adding variable descriptions
    
    * DR-537 Sorting variable definitions
    
    * DR-537 Fixed inline policy
    
    * DR-537 Added unit tests
    
    * DR-537 Removed failing unit tests
    
    * DR-537 Removed failing unit tests
    
    * DR-537 Added locations data load in to units test
    
    * DR-537 Fixed dir in unit test script
    
    * DR-537 Updated pandas in to requirements file
    
    * DR-537 Removed reliance on main python file
    
    * DR-537 Removed reliance on main python file
    
    * DR-537 Another fix in test
    
    * DR-537 Complete unit test code
    
    * DR-537 Removed init file
    
    * DR-537 Updated requirements file
    
    * DR-537 Added more unit tests and some fixes
    
    * DR-537 Added more unit tests and some fixes
    
    * DR-537 Added more unit tests and some fixes
    
    * DR-537 Added more unit tests and some fixes
    
    * DR-537 Added more unit tests and some fixes
    
    * DR-537 Updated exclude statement in lambda tf
    
    * DR-537 Updated exclude statement in lambda tf
    
    * DR-537 Fixed few issues flagged by sonarcloud
    
    * DR-537 Fixed few issues flagged by sonarcloud
    
    * DR-537 Fixed few issues flagged by sonarcloud
    
    * DR-537 Fixed few issues flagged by sonarcloud
    
    * DR-537 Fixed few issues flagged by sonarcloud
    
    * DR-537 Added more unit tests
    
    * DR-537 Added lambda handler test
    
    * DR-537 Added lambda handler test
    
    * DR-537 Moved code around to reduce duplication
    
    * DR-537 Parameter store entry for api url and unit test updates
    
    * DR-537 Parameter store entry for api url and unit test updates

---
+++ infrastructure/stacks/support_tools/locations_lambda.tf
@@ -0,0 +1,79 @@
+locals {
+  source_dir = "../../../scripts/locations_data_load"
+}
+
+
+data "archive_file" "locations_lambda_deployment_file" {
+  type        = "zip"
+  source_dir  = local.source_dir
+  excludes    = setsubtract(fileset("${local.source_dir}/", "*"), ["ODS_Codes.xlsx", "locations_lambda.py"])
+  output_path = "${local.source_dir}/locations_lambda.zip"
+}
+
+module "locations-lambda" {
+  source = "../../modules/lambda"
+
+  function_name = "locations_lambda"
+  description   = "To pull ODS locations data nad write to DynamoDB Locations table"
+  handler       = "locations_lambda.lambda_handler"
+
+  local_existing_package = data.archive_file.locations_lambda_deployment_file.output_path
+
+  policy_jsons = [
+    <<-EOT
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "ListParameters",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:DescribeParameters"
+                    ],
+                    "Resource": "*"
+                },
+                {
+                    "Sid": "UseParameters",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:GetParameters"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:${var.aws_region}:${local.account_id}:parameter/data/api/lambda/client_id",
+                        "arn:aws:ssm:${var.aws_region}:${local.account_id}:parameter/data/api/lambda/client_secret"
+                    ]
+                },
+                {
+                    "Sid": "AliasBasedKMS",
+                    "Effect": "Allow",
+                    "Action": [
+                      "kms:Decrypt",
+                      "kms:Encrypt",
+                      "kms:GenerateDataKey*",
+                      "kms:DescribeKey"
+                    ],
+                    "Resource": "arn:aws:kms:${var.aws_region}:${local.account_id}:key/*",
+                    "Condition": {
+                      "StringEquals": {
+                        "kms:ResourceAliases": "alias/aws/ssm"
+                      }
+                    }
+                },
+                {
+                    "Sid": "DynamodbTable",
+                    "Effect": "Allow",
+                    "Action": [
+                        "dynamodb:PutItem",
+                        "dynamodb:DeleteItem",
+                        "dynamodb:GetItem",
+                        "dynamodb:Scan",
+                        "dynamodb:Query",
+                        "dynamodb:UpdateItem"
+                    ],
+                    "Resource": "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/locations"
+                }
+            ]
+        }
+        EOT
+  ]
+}
