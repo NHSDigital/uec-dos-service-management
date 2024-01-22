cc4d0bce8a6eab9f1fae2ffa971cdcea02913d5d
Tim Rickwood <tim.rickwood@nhs.net>
2024-01-22 13:17:27 +0000

    DR-646 Add depends on (#74)

--- infrastructure/stacks/application/apig-organisations.tf
+++ infrastructure/stacks/application/apig-organisations.tf
@@ -116,6 +116,10 @@ module "organisations_aws_lambda_permission" {
   account_id           = local.account_id
   lambda_function_name = var.organisations_function_name
   rest_api_id          = module.sm_rest_api.rest_api_id
+
+  depends_on = [
+    module.organisations-data-manager-lambda
+  ]
 }
 
 #####
