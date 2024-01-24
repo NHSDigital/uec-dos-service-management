61c8aaf7b0eab057417ac48cc7103fe7afc73f3f
Tim Rickwood <tim.rickwood@nhs.net>
2024-01-24 16:17:25 +0000

    DR-659 Add security group for aurora

---
+++ infrastructure/stacks/support_tools/data.tf
@@ -0,0 +1,18 @@
+# "aws_vpc" "main"
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.project}-${var.vpc_name}-${var.environment}"]
+  }
+}
+
+data "aws_ec2_client_vpn_endpoint" "service_management_vpn" {
+  filter {
+    name   = "transport-protocol"
+    values = ["tcp"]
+  }
+}
+
+data "aws_security_group" "vpn_secgroup" {
+    name = var.vpn_security_group_name
+}
---
+++ infrastructure/stacks/support_tools/security_group.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "aurora_sg" {
+  name        = "aurora-sg"
+  description = "Security group for aurora"
+  vpc_id      = data.aws_vpc.main.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "aurora_ingress" {
+  security_group_id = aws_security_group.aurora_sg.id
+
+  referenced_security_group_id = data.aws_security_group.vpn_secgroup.arn
+  from_port                    = 5432
+  ip_protocol                  = data.aws_ec2_client_vpn_endpoint.service_management_vpn.transport_protocol
+  to_port                      = 5432
+}
