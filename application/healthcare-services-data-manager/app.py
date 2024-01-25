e64ad77b099efcb078fb47939536c6453aa072c7
Jonathan Pearce <j.pearce1@nhs.net>
2024-01-25 14:39:11 +0000

    DR-999 Test sonar prob

--- application/healthcare-services-data-manager/app.py
+++ application/healthcare-services-data-manager/app.py
@@ -31,7 +31,7 @@ def get_healthcareservices():
 @app.put("/healthcare_services")
 def update_healthcareservices():
     put_data: dict = app.current_event.json_body
-    response = service.update_record(put_data)
+    response = service.update_record(put_data, "hey", "hey2")
     return response
 
 
