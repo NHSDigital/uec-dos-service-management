9364e6d47fbc7fdd96c3564f68ea7b5e47bd7f6e
stuartreid <stuart.reid5@nhs.net>
2024-01-25 15:57:04 +0000

    DR-660 Aurora private zone and cname record

---
+++ infrastructure/stacks/network/route53.tf
@@ -0,0 +1,14 @@
+resource "aws_route53_zone" "private" {
+  name = "uec-dos-mig-dev"
+  vpc {
+    vpc_id = aws_vpc.main.id
+  }
+}
+
+resource "aws_route53_record" "dev" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "mig-datastore"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["aws_rds_cluster_instance"] #dummy endpoint until known
+}
