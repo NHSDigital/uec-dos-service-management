cd4f2ebb232e92bfe497bfd5880756a28a414eca
Tamara Goldschmidt <77108863+tamaragoldschmidt@users.noreply.github.com>
2024-01-31 13:17:56 +0000

    DR-510 Clean script checks (#83)
    * DR-510 Cleansing and checking scripts
    
    * DR-510 Formatting changes made
    
    * DR-510 Added in service notes

---
+++ scripts/sql/sanitisation_scripts/Cleanse_Data_Script.sql
@@ -0,0 +1,115 @@
+-- Script to clean production data for use in non-prod environments
+
+
+-- 1.0 Truncate User related Tables
+
+truncate pathwaysdos.savedsearches, pathwaysdos.usersavedsearches;
+truncate pathwaysdos.serviceattributes cascade;
+truncate pathwaysdos.servicenotes;
+
+-- 1.1 Clear down accounts
+delete from pathwaysdos.userpermissions up where up.userid not in (31);
+delete from pathwaysdos.userreferralroles ur where ur.userid not in (31);
+delete from pathwaysdos.userregions ur where ur.userid not in (31);
+delete from pathwaysdos.userservices us where us.userid not in (31);
+delete from pathwaysdos.users u where u.id not in (31);
+
+--- run separate script to update user 31
+
+-- 2.0 Stub Sensitive Fields In The Services Table
+update pathwaysdos.services
+set publicreferralinstructions = concat('STUB Public Referral Instruction Text Field ', id)
+where publicreferralinstructions is not null;
+
+update pathwaysdos.services
+set telephonetriagereferralinstructions = concat('STUB Telephone Triage Referral Instructions Text Field ', id)
+where telephonetriagereferralinstructions is not null;
+
+update pathwaysdos.services set nonpublicphone = '99999 000000' where nonpublicphone is not null AND (id % 2) = 0;
+
+update pathwaysdos.services set nonpublicphone = '00000 888888' where nonpublicphone is not null AND (id % 2) = 1;
+
+update pathwaysdos.services set fax = '77777 000000' where fax is not null AND (id % 2) = 0;
+
+update pathwaysdos.services set fax = '00000 666666' where fax is not null AND (id % 2) = 1;
+
+update pathwaysdos.services set email = concat(id, '-fake@nhs.gov.uk') where email is not null;
+
+update pathwaysdos.services set createdby = 'HUMAN' where createdby != 'ROBOT';
+
+update pathwaysdos.services set modifiedby = 'HUMAN' where modifiedby != 'ROBOT';
+
+-- 3.0 Stub Possibly Sensitive Fields In The Service Capacities Table
+update pathwaysdos.servicecapacities set notes = null, modifiedbyid = null, modifiedby = null,
+modifieddate = null, capacitystatusid = 1;
+
+-- 4.1 Truncate grid tables
+truncate pathwaysdos.capacitygridsheethistories;
+truncate pathwaysdos.capacitygridconditionalstyles, pathwaysdos.capacitygridcustomformulas, pathwaysdos.capacitygridcustomformulastyles,
+pathwaysdos.capacitygriddata, pathwaysdos.capacitygridheaders, pathwaysdos.capacitygridparentsheets, pathwaysdos.capacitygridservicetypes;
+truncate pathwaysdos.servicecapacitygrids;
+
+-- 4.2 Truncate news and news related data
+truncate pathwaysdos.news, pathwaysdos.newsacknowledgedbyusers, pathwaysdos.newsforpermissions;
+
+-- 4.3 Truncate purgedusers
+truncate pathwaysdos.purgedusers;
+
+-- 4.4 Truncate Service Change History Tables and The Service Endpoints Table
+truncate pathwaysdos.changes, pathwaysdos.servicehistories;
+truncate pathwaysdos.serviceendpoints cascade;
+
+-- 5.0 Stub Sensitive Fields in the Service Phone Numbers table
+
+update pathwaysdos.servicephonenumbers set phonenumber = '99999 000000', phonedescription = concat('STUB Phonedescription NonPublic 1  Text Field ' , id) where ispublic=false AND (id % 2) = 0;
+
+update pathwaysdos.servicephonenumbers set phonenumber = '00000 888888', phonedescription = concat('STUB Phonedescription NonPublic 2  Text Field ' , id)  where ispublic=false AND (id % 2) = 1;
+
+update pathwaysdos.servicephonenumbers set phonenumber = '888888 000000', phonedescription = concat('STUB Phonedescription Public 1  Text Field ' , id)   where ispublic=true AND (id % 2) = 0;
+
+update pathwaysdos.servicephonenumbers set phonenumber = '00000 999999', phonedescription = concat('STUB Phonedescription Public 2  Text Field ' , id)   where ispublic=true AND (id % 2) = 1;
+
+
+-- 6.0 Stub out Sensitive Professionalreferralinfo in Services table to contain realistic characters and length
+update pathwaysdos.services  set professionalreferralinfo = '# **Service Information**
+
+The flu vaccine is a safe and effective vaccine.
+It''s offered every year on the NHS to help protect people at risk of getting seriously ill from flu.
+
+**Information Links:**
+
+Adult''s flu vaccine - https://www.nhs.uk/conditions/vaccinations/flu-influenza-vaccine/
+
+Children''s flu vaccine - https://www.nhs.uk/conditions/vaccinations/child-flu-vaccine/
+
+Made Up Links  - https://www.nhs.uk/service-search/pharmacy/find-a-pharmacy/download.cfm?doc=docm93jijm4n19386.docx&ver=51796
+
+The best time to have the flu vaccine is in the autumn or early winter before flu starts spreading. But you can get the vaccine later.
+# **Where to get the flu vaccine?**
+You can have the NHS flu vaccine at:
+* Your GP surgery
+* A pharmacy offering the service
+* Your midwifery service if you''re pregnant
+* A hospital appointment
+If you do not have your flu vaccine at your GP surgery, you do not have to tell the surgery. This will be done for you.
+
+# **Who can have the flu vaccine?**
+The flu vaccine is given free on the NHS to people who:
+* Are 50 and over (including those who''ll be 50 by 31 March 2022)
+* Have certain health conditions
+* Are pregnant
+* Are in long-stay residential care
+* Receive a carer''s allowance, or are the main carer for an older or disabled person who may be at risk if you get sick
+* Live with someone who is more likely to get infections (such as someone who has HIV, has had a transplant or is having certain treatments for cancer, lupus or rheumatoid arthritis)
+* Frontline health or social care workers
+
+# Who should not have the flu vaccine?
+Most adults can have the flu vaccine, but you should avoid it if you have had a serious allergic reaction to a flu vaccine in the past.
+
+You may be at risk of an allergic reaction to the flu vaccine injection if you have an egg allergy. This is because some flu vaccines are made using eggs.
+
+Ask a GP or pharmacist for a low-egg or egg-free vaccine.
+
+If you''re ill with a high temperature, it''s best to wait until you''re better before having the flu vaccine.
+
+' where professionalreferralinfo is not null
---
+++ scripts/sql/sanitisation_scripts/Post_sanitise_data_check.sql
@@ -0,0 +1,189 @@
+DROP TABLE IF EXISTS  temp_script_counts;
+create TEMP TABLE temp_script_counts(
+count int, indicator varchar(50), stage  varchar(50)
+);
+
+insert into temp_script_counts
+select count(*) as count, 'servicenotes_truncated_table' as indicator, 'after' as stage from pathwaysdos.servicenotes;
+insert into temp_script_counts
+select count(*) as count, 'savedsearches_truncated_table' as indicator, 'after' as stage from pathwaysdos.savedsearches;
+insert into temp_script_counts
+select count(*) as count, 'usersavedsearches_truncated_table' , 'after' as stage from pathwaysdos.usersavedsearches ;
+insert into temp_script_counts
+select count(*) as count, 'userpermissions_truncated_table' , 'after' as stage from  pathwaysdos.userpermissions up where up.userid not in (31,13608);
+insert into temp_script_counts
+select count(*) as count, 'userreferralroles_truncated_table' , 'after' as stage from pathwaysdos.userreferralroles ur where ur.userid not in (31,13608);
+insert into temp_script_counts
+select count(*) as count, 'userregion_truncated_tables' , 'after' as stage from pathwaysdos.userregions ur where ur.userid not in (31,13608);
+insert into temp_script_counts
+select count(*) as count, 'userservices_truncated_table' , 'after' as stage from pathwaysdos.userservices us where us.userid not in (31,13608);
+insert into temp_script_counts
+select count(*) as count, 'users_to only_leave_admin' , 'after' as stage from pathwaysdos.users u where u.id not in (31,13608);
+insert into temp_script_counts
+select count(*) as count, 'capacitygridsheethistories_truncated_table' , 'after' as stage from pathwaysdos.capacitygridsheethistories;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridconditionalstyles_truncated_table' , 'after' as stage from pathwaysdos.capacitygridconditionalstyles;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridcustomformulas_truncated_table' , 'after' as stage from pathwaysdos.capacitygridcustomformulas;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridcustomformulastyles_truncated_table' , 'after' as stage from pathwaysdos.capacitygridcustomformulastyles;
+insert into temp_script_counts
+select count(*) as count, 'capacitygriddata_truncated_table' , 'after' as stage from pathwaysdos.capacitygriddata;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridheaders_truncated_table' , 'after' as stage from pathwaysdos.capacitygridheaders;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridparentsheets' , 'after' as stage from pathwaysdos.capacitygridparentsheets;
+insert into temp_script_counts
+select count(*) as count, 'capacitygridservicetypes_truncated_table' , 'after' as stage from pathwaysdos.capacitygridservicetypes;
+insert into temp_script_counts
+select count(*) as count, 'servicecapacitygrids_truncated_table' , 'after' as stage from pathwaysdos.servicecapacitygrids;
+insert into temp_script_counts
+select count(*) as count, 'news_truncated_table' , 'after' as stage from pathwaysdos.news;
+insert into temp_script_counts
+select count(*) as count, 'newsacknowledgedbyusers_truncated_table' , 'after' as stage from pathwaysdos.newsacknowledgedbyusers;
+insert into temp_script_counts
+select count(*) as count, 'newsforpermissions_truncated_table' , 'after' as stage from pathwaysdos.newsforpermissions;
+insert into temp_script_counts
+select count(*) as count, 'purgedusers_truncated_table' , 'after' as stage from pathwaysdos.purgedusers;
+insert into temp_script_counts
+select count(*) as count, 'changes_truncated_table' , 'after' as stage from pathwaysdos.changes;
+insert into temp_script_counts
+select count(*) as count, 'servicehistories_truncated_table' , 'after' as stage from pathwaysdos.servicehistories;
+insert into temp_script_counts
+select count(*) as count, 'serviceendpoints_truncated_table' , 'after' as stage from pathwaysdos.serviceendpoints;
+
+insert into temp_script_counts
+select count(*) as count, 'publicreferralinstructions_update_field' , 'after' as stage from pathwaysdos.services where publicreferralinstructions is not null
+and publicreferralinstructions = concat('STUB Public Referral Instruction Text Field ', id);
+
+insert into temp_script_counts
+select count(*) as count, 'telephonetriagereferralinstructions_update_field' , 'after' as stage from pathwaysdos.services where telephonetriagereferralinstructions is not null
+and telephonetriagereferralinstructions = concat('STUB Telephone Triage Referral Instructions Text Field ', id);
+
+
+insert into temp_script_counts
+select count(*) as count, 'nonpublicphone_1_update_field' , 'after' as stage from pathwaysdos.services where nonpublicphone is not null AND (id % 2) = 0
+and nonpublicphone = '99999 000000';
+
+insert into temp_script_counts
+select count(*) as count, 'nonpublicphone_2_update_field' , 'after' as stage from pathwaysdos.services where nonpublicphone is not null AND (id % 2) = 1
+and nonpublicphone = '00000 888888';
+
+insert into temp_script_counts
+select count(*) as count, 'nonpublicphone_total' , 'after' as stage from pathwaysdos.services where nonpublicphone is not null and nonpublicphone in ('00000 888888','99999 000000');
+
+
+insert into temp_script_counts
+select count(*) as count, 'fax_1_update_field' , 'after' as stage from pathwaysdos.services where fax is not null AND (id % 2) = 0
+and fax = '77777 000000';
+
+insert into temp_script_counts
+select count(*) as count, 'fax_2_update_field' , 'after' as stage from pathwaysdos.services where fax is not null AND (id % 2) = 1
+and fax = '00000 666666';
+
+insert into temp_script_counts
+select count(*) as count, 'fax_total' , 'after' as stage from pathwaysdos.services where fax is not null and fax in ( '00000 666666', '77777 000000') ;
+
+
+insert into temp_script_counts
+select count(*) as count, 'email_update_field' , 'after' as stage from pathwaysdos.services where email is not null
+and email = concat(id, '-fake@nhs.gov.uk');
+
+insert into temp_script_counts
+select count(*) as count, 'createdby_update_field' , 'after' as stage from pathwaysdos.services where createdby != 'ROBOT'
+and createdby = 'HUMAN';
+
+insert into temp_script_counts
+select count(*) as count, 'createdby_ROBOT' , 'after' as stage from pathwaysdos.services where createdby = 'ROBOT';
+
+insert into temp_script_counts
+select count(*) as count, 'modifiedby_update_field' , 'after' as stage from pathwaysdos.services where modifiedby != 'ROBOT'
+and modifiedby = 'HUMAN';
+
+insert into temp_script_counts
+select count(*) as count, 'modifiedby_ROBOT' , 'after' as stage from pathwaysdos.services where modifiedby = 'ROBOT';
+
+insert into temp_script_counts
+select count(*) as count, 'notes_update_field' , 'after' as stage from pathwaysdos.servicecapacities where notes is null;
+
+insert into temp_script_counts
+select count(*) as count, 'modifiedbyid_update_field' , 'after' as stage from pathwaysdos.servicecapacities where modifiedbyid is null;
+
+insert into temp_script_counts
+select count(*) as count, 'modifiedby_update_field' , 'after' as stage from pathwaysdos.servicecapacities where modifiedby is null;
+
+insert into temp_script_counts
+select count(*) as count, 'modifieddat_update_fielde' , 'after' as stage from pathwaysdos.servicecapacities where modifieddate is  null;
+
+
+insert into temp_script_counts
+select count(*) as count, 'capacitystatusid_total' , 'after' as stage from pathwaysdos.servicecapacities ;
+
+insert into temp_script_counts
+select count(*) as count, 'capacitystatusid_not_1_update_field' , 'after' as stage from pathwaysdos.servicecapacities where capacitystatusid = 1 ;
+
+
+insert into temp_script_counts
+select count(*) as count, 'servicephonenumbers_nonpublic_1_update_field' , 'after' as stage from pathwaysdos.servicephonenumbers where ispublic=false AND (id % 2) = 0
+and phonenumber = '99999 000000' and phonedescription = concat('STUB Phonedescription NonPublic 1  Text Field ' , id);
+
+insert into temp_script_counts
+select count(*) as count, 'servicephonenumbers_nonpublic_2_update_field' , 'after' as stage from pathwaysdos.servicephonenumbers where ispublic=false AND (id % 2) = 1
+and phonenumber = '00000 888888' and  phonedescription = concat('STUB Phonedescription NonPublic 2  Text Field ' , id);
+
+insert into temp_script_counts
+select count(*) as count, 'servicephonenumbers_public_1_update_field' , 'after' as stage from pathwaysdos.servicephonenumbers where ispublic=true AND (id % 2) = 0
+and phonenumber = '888888 000000'and phonedescription = concat('STUB Phonedescription Public 1  Text Field ' , id) ;
+
+
+insert into temp_script_counts
+select count(*) as count, 'servicephonenumbers_public_1_update_field' , 'after' as stage from pathwaysdos.servicephonenumbers where ispublic=true AND (id % 2) = 1
+and phonenumber = '00000 999999'and phonedescription = concat('STUB Phonedescription Public 2  Text Field ' , id) ;
+
+insert into temp_script_counts
+select count(*) as count, 'professionalreferralinfo_update_field' , 'after' as stage from pathwaysdos.services where professionalreferralinfo is not null
+and
+professionalreferralinfo = '# **Service Information**
+
+The flu vaccine is a safe and effective vaccine.
+It''s offered every year on the NHS to help protect people at risk of getting seriously ill from flu.
+
+**Information Links:**
+
+Adult''s flu vaccine - https://www.nhs.uk/conditions/vaccinations/flu-influenza-vaccine/
+
+Children''s flu vaccine - https://www.nhs.uk/conditions/vaccinations/child-flu-vaccine/
+
+Made Up Links  - https://www.nhs.uk/service-search/pharmacy/find-a-pharmacy/download.cfm?doc=docm93jijm4n19386.docx&ver=51796
+
+The best time to have the flu vaccine is in the autumn or early winter before flu starts spreading. But you can get the vaccine later.
+# **Where to get the flu vaccine?**
+You can have the NHS flu vaccine at:
+* Your GP surgery
+* A pharmacy offering the service
+* Your midwifery service if you''re pregnant
+* A hospital appointment
+If you do not have your flu vaccine at your GP surgery, you do not have to tell the surgery. This will be done for you.
+
+# **Who can have the flu vaccine?**
+The flu vaccine is given free on the NHS to people who:
+* Are 50 and over (including those who''ll be 50 by 31 March 2022)
+* Have certain health conditions
+* Are pregnant
+* Are in long-stay residential care
+* Receive a carer''s allowance, or are the main carer for an older or disabled person who may be at risk if you get sick
+* Live with someone who is more likely to get infections (such as someone who has HIV, has had a transplant or is having certain treatments for cancer, lupus or rheumatoid arthritis)
+* Frontline health or social care workers
+
+# Who should not have the flu vaccine?
+Most adults can have the flu vaccine, but you should avoid it if you have had a serious allergic reaction to a flu vaccine in the past.
+
+You may be at risk of an allergic reaction to the flu vaccine injection if you have an egg allergy. This is because some flu vaccines are made using eggs.
+
+Ask a GP or pharmacist for a low-egg or egg-free vaccine.
+
+If you''re ill with a high temperature, it''s best to wait until you''re better before having the flu vaccine.
+
+'  ;
+
+select stage, indicator, count  from temp_script_counts;
