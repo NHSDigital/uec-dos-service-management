e7d23a999150f1b9c99373f8c048eacb95d1d89e
mindaugas-nhsdos <mindaugas.petrauskas1@nhs.net>
2024-02-01 17:00:36 +0000

    DR-598 Preparing files to remove duplication

--- scripts/org_data_load/test/test_org_data_load.py
+++ scripts/org_data_load/test/test_org_data_load.py
@@ -1,72 +1,19 @@
 from scripts.org_data_load.deploy.org_data_load import (
     lambda_handler,
-    get_ssm,
-    get_api_token,
-    get_headers,
-    capitalize_address_item,
-    capitalize_line,
     read_excel_values,
-    read_ods_api,
     write_to_dynamodb,
-    generate_random_id,
-    get_formatted_datetime,
     process_organization,
-    process_pharmacy,
-    process_non_pharmacy,
+    process_attributes,
     process_organizations,
     process_type,
     data_exists,
     update_records,
     fetch_organizations,
-    get_table_name,
 )
 
-import os
 import unittest
 from unittest.mock import patch, MagicMock, Mock
-from unittest import mock
 import pandas as pd
-import datetime
-
-
-# Set Test variables
-workspace_env_name = "WORKSPACE"
-workspace_env_value = "DR-000"
-workspace_env_default_value = ""
-test_table_name = "TEST-TABLE"
-
-
-def test_get_table_name_with_no_environment_var():
-    # Test get_table_name method without WORKSPACE environment variable set
-    table_name = get_table_name(test_table_name)
-    assert table_name == test_table_name
-
-
-def test_get_table_name_with_no_environment_var_empty_table_name():
-    # Test get_table_name method without WORKSPACE environment variable set and empty table name
-    table_name = get_table_name("")
-    assert table_name == ""
-
-
-@mock.patch.dict(os.environ, {workspace_env_name: workspace_env_default_value})
-def test_get_table_name_with_environment_var_empty():
-    # Test get_table_name method with WORKSPACE environment variable empty"
-    table_name = get_table_name(test_table_name)
-    assert table_name == test_table_name
-
-
-@mock.patch.dict(os.environ, {workspace_env_name: workspace_env_value})
-def test_get_table_name_with_environment_var_set():
-    # Test get_table_name method with WORKSPACE environment variable set"
-    table_name = get_table_name(test_table_name)
-    assert table_name == test_table_name + "-" + workspace_env_value
-
-
-@mock.patch.dict(os.environ, {workspace_env_name: workspace_env_value})
-def test_get_table_name_with_environment_var_set_empty_table_name():
-    # Test get_table_name method with WORKSPACE environment variable set with no table name set"
-    table_name = get_table_name("")
-    assert table_name == "-" + workspace_env_value
 
 
 class TestLambdaHandler(unittest.TestCase):
@@ -81,104 +28,6 @@ class TestLambdaHandler(unittest.TestCase):
 
         # Assertions
         mock_fetch_organizations.assert_called_once()
-
-
-class TestSsmGetTokenGetHeaders(unittest.TestCase):
-    @patch("scripts.org_data_load.deploy.org_data_load.boto3.client")
-    def test_get_ssm(self, mock_boto3_client):
-        # Mocking boto3.client to avoid actual AWS calls
-        mock_ssm_client = MagicMock()
-        mock_boto3_client.return_value = mock_ssm_client
-
-        # Mocking the response of get_parameter
-        parameter_value = "mocked_value"
-        mock_ssm_client.get_parameter.return_value = {
-            "Parameter": {"Value": parameter_value}
-        }
-
-        # Call the function with a mock parameter name
-        result = get_ssm("mocked_parameter_name")
-
-        # Assert that the mocked values were used and the function behaves as expected
-        mock_boto3_client.assert_called_once_with("ssm")
-        mock_ssm_client.get_parameter.assert_called_once_with(
-            Name="mocked_parameter_name", WithDecryption=True
-        )
-        self.assertEqual(result, parameter_value)
-
-    @patch("scripts.org_data_load.deploy.org_data_load.get_ssm")
-    @patch("scripts.org_data_load.deploy.org_data_load.requests.post")
-    def test_get_api_token(self, mock_post, mock_get_ssm):
-        # Mocking the SSM calls
-        mock_get_ssm.side_effect = lambda x: f"mocked_ssm_value{x}"
-
-        # Mocking the response of the requests.post call
-        mock_response = MagicMock()
-        mock_response.json.return_value = {"access_token": "mocked_access_token"}
-        mock_post.return_value = mock_response
-
-        # Call the function
-        result = get_api_token()
-
-        # Assert that the mocked values were used and the function behaves as expected
-        mock_post.assert_called_once_with(
-            url="mocked_ssm_value/data/api/lambda/ods/domain"
-            "//authorisation/auth/realms/terminology/protocol/openid-connect/token",
-            headers={
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Accept": "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Connection": "Keep-alive",
-            },
-            data={
-                "grant_type": "client_credentials",
-                "client_id": "mocked_ssm_value/data/api/lambda/client_id",
-                "client_secret": "mocked_ssm_value/data/api/lambda/client_secret",
-            },
-        )
-        self.assertEqual(result, "mocked_access_token")
-
-    @patch("scripts.org_data_load.deploy.org_data_load.get_api_token")
-    def test_get_headers(self, mock_get_api_token):
-        # Mocking the get_api_token call
-        mock_get_api_token.return_value = "mocked_access_token"
-
-        # Call the function
-        result = get_headers()
-
-        # Assert that the mocked values were used and the function behaves as expected
-        mock_get_api_token.assert_called_once()
-        self.assertEqual(result, {"Authorization": "Bearer mocked_access_token"})
-
-
-class TestCapitalizeLine(unittest.TestCase):
-    def test_capitalize_line(self):
-        result = capitalize_line("hello world")
-        self.assertEqual(result, "Hello World")
-
-
-class TestCapitalizeAddressItem(unittest.TestCase):
-    def test_capitalize_address_item(self):
-        address_item = {
-            "line": ["123 main street", "apt 4"],
-            "city": "example city",
-            "district": "example district",
-            "country": "example country",
-            "postalCode": "12345",
-            "extension": {"key": "value"},
-        }
-
-        result = capitalize_address_item(address_item)
-
-        expected_result = {
-            "line": ["123 Main Street", "Apt 4"],
-            "city": "Example City",
-            "district": "Example District",
-            "country": "Example Country",
-            "postalCode": "12345",
-        }
-
-        self.assertEqual(result, expected_result)
 
 
 class TestReadExcelValues(unittest.TestCase):
@@ -211,54 +60,6 @@ class TestReadExcelValues(unittest.TestCase):
             },
         ]
         self.assertEqual(result, expected_params)
-
-
-class TestReadODSAPI(unittest.TestCase):
-    @patch("scripts.org_data_load.deploy.org_data_load.requests.get")
-    def test_read_ods_api_success(self, mock_requests_get):
-        params = {"param1": "value1", "param2": "value2"}
-        # Mocking get_api_token to return a dummy token
-        headers = {"Authorization": "Bearer dummy_token"}
-        # Mocking requests.get to return a successful response
-        mock_response = MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {"data": "dummy_data"}
-        mock_requests_get.return_value = mock_response
-        # Call the function with dummy parameters
-        result = read_ods_api("dummy_api_endpoint", headers, params)
-
-        # Assert that requests.get was called with the expected arguments
-        mock_requests_get.assert_called_once_with(
-            "dummy_api_endpoint",
-            headers={"Authorization": "Bearer dummy_token"},
-            params={"param1": "value1", "param2": "value2"},
-        )
-        # Assert that the result is as expected
-        self.assertEqual(result, {"data": "dummy_data"})
-
-    @patch("scripts.org_data_load.deploy.org_data_load.requests.get")
-    def test_read_ods_api_failure(self, mock_requests_get):
-        params = {"param1": "value1", "param2": "value2"}
-        headers = {"Authorization": "Bearer dummy_token"}
-
-        # Mocking requests.get to return a failed response
-        mock_response = MagicMock()
-        mock_response.status_code = 404
-        mock_response.text = "Not Found"
-        mock_requests_get.return_value = mock_response
-
-        # Call the function with dummy parameters
-        result = read_ods_api("dummy_api_endpoint", headers, params)
-
-        # Assert that requests.get was called with the expected arguments
-        mock_requests_get.assert_called_once_with(
-            "dummy_api_endpoint",
-            headers={"Authorization": "Bearer dummy_token"},
-            params={"param1": "value1", "param2": "value2"},
-        )
-
-        # Assert that the result is None in case of failure
-        self.assertIsNone(result)
 
 
 class TestWriteToDynamoDB(unittest.TestCase):
@@ -347,34 +148,6 @@ class TestWriteToDynamoDB(unittest.TestCase):
         )
 
 
-class TestGenerateRandomId(unittest.TestCase):
-    def test_generate_random_id_length(self):
-        # Call the function to generate a random ID
-        random_id = generate_random_id()
-
-        # Assert that the length of the generated ID is 16
-        self.assertEqual(len(random_id), 16)
-
-
-class TestGetFormattedDatetime(unittest.TestCase):
-    def test_get_formatted_datetime_format(self):
-        # Call the function to get the formatted datetime
-        formatted_datetime = get_formatted_datetime()
-
-        # Define the expected format
-        expected_format = "%d-%m-%Y %H:%M:%S"
-
-        # Use try-except block to catch any exceptions raised by strptime
-        try:
-            # Try to parse the formatted datetime using the expected format
-            datetime.datetime.strptime(formatted_datetime, expected_format)
-        except ValueError:
-            # If parsing fails, raise an AssertionError
-            self.fail(
-                f"Formatted datetime '{formatted_datetime}' does not match the expected format '{expected_format}'."
-            )
-
-
 class TestProcessOrganization(unittest.TestCase):
     def test_process_organization_with_pharmacy_headquarter(self):
         # Mock data with a pharmacy headquarter organization
@@ -416,7 +189,7 @@ class TestProcessPharmacy(unittest.TestCase):
     @patch("scripts.org_data_load.deploy.org_data_load.generate_random_id")
     @patch("scripts.org_data_load.deploy.org_data_load.process_type")
     @patch("scripts.org_data_load.deploy.org_data_load.get_formatted_datetime")
-    def test_process_pharmacy_with_ph_org(
+    def test_process_attributes_with_ph_org(
         self,
         mock_get_formatted_datetime,
         mock_process_type,
@@ -444,7 +217,7 @@ class TestProcessPharmacy(unittest.TestCase):
         mock_get_formatted_datetime.return_value = "formatted_datetime"
 
         # Call the function to process pharmacy
-        result = process_pharmacy(org, ph_org)
+        result = process_attributes(org, ph_org)
 
         # Assert that the result has the expected attributes based on the mock data
         self.maxDiff = None
@@ -473,7 +246,7 @@ class TestProcessPharmacy(unittest.TestCase):
     @patch("scripts.org_data_load.deploy.org_data_load.generate_random_id")
     @patch("scripts.org_data_load.deploy.org_data_load.process_type")
     @patch("scripts.org_data_load.deploy.org_data_load.get_formatted_datetime")
-    def test_process_pharmacy_without_ph_org(
+    def test_process_attributes_without_ph_org(
         self,
         mock_get_formatted_datetime,
         mock_process_type,
@@ -501,7 +274,7 @@ class TestProcessPharmacy(unittest.TestCase):
         mock_get_formatted_datetime.return_value = "formatted_datetime"
 
         # Call the function to process pharmacy
-        result = process_pharmacy(org, ph_org)
+        result = process_attributes(org, ph_org)
 
         # Assert that the result has the expected attributes based on the mock data
         self.assertEqual(
@@ -526,59 +299,59 @@ class TestProcessPharmacy(unittest.TestCase):
         )
 
 
-class TestProcessNonPharmacy(unittest.TestCase):
-    @patch("scripts.org_data_load.deploy.org_data_load.capitalize_address_item")
-    @patch("scripts.org_data_load.deploy.org_data_load.generate_random_id")
-    @patch("scripts.org_data_load.deploy.org_data_load.process_type")
-    @patch("scripts.org_data_load.deploy.org_data_load.get_formatted_datetime")
-    def test_process_non_pharmacy(
-        self,
-        mock_get_formatted_datetime,
-        mock_process_type,
-        mock_generate_random_id,
-        mock_capitalize_address_item,
-    ):
-        # Mock data for a non-pharmacy organization
-        org = {
-            "resourceType": "Organization",
-            "id": "org_id",
-            "name": "Non-Pharmacy Name",
-            "type": "Some Type",
-            "address": [{"street": "456 Second St", "city": "City", "state": "State"}],
-        }
+# class TestProcessNonPharmacy(unittest.TestCase):
+#     @patch("scripts.org_data_load.deploy.org_data_load.capitalize_address_item")
+#     @patch("scripts.org_data_load.deploy.org_data_load.generate_random_id")
+#     @patch("scripts.org_data_load.deploy.org_data_load.process_type")
+#     @patch("scripts.org_data_load.deploy.org_data_load.get_formatted_datetime")
+#     def test_process_non_pharmacy(
+#         self,
+#         mock_get_formatted_datetime,
+#         mock_process_type,
+#         mock_generate_random_id,
+#         mock_capitalize_address_item,
+#     ):
+#         # Mock data for a non-pharmacy organization
+#         org = {
+#             "resourceType": "Organization",
+#             "id": "org_id",
+#             "name": "Non-Pharmacy Name",
+#             "type": "Some Type",
+#             "address": [{"street": "456 Second St", "city": "City", "state": "State"}],
+#         }
 
-        # Mock the functions and methods used within process_non_pharmacy
-        mock_capitalize_address_item.return_value = {
-            "street": "456 Second St",
-            "city": "City",
-            "state": "State",
-        }
-        mock_generate_random_id.return_value = "generated_id"
-        mock_process_type.return_value = "Processed Type"
-        mock_get_formatted_datetime.return_value = "formatted_datetime"
+#         # Mock the functions and methods used within process_non_pharmacy
+#         mock_capitalize_address_item.return_value = {
+#             "street": "456 Second St",
+#             "city": "City",
+#             "state": "State",
+#         }
+#         mock_generate_random_id.return_value = "generated_id"
+#         mock_process_type.return_value = "Processed Type"
+#         mock_get_formatted_datetime.return_value = "formatted_datetime"
 
-        # Call the function to process non-pharmacy organization
-        result = process_non_pharmacy(org)
+#         # Call the function to process non-pharmacy organization
+#         result = process_non_pharmacy(org)
 
-        # Assert that the result has the expected attributes based on the mock data
-        self.assertEqual(
-            result,
-            {
-                "resourceType": "Organization",
-                "id": "generated_id",
-                "identifier": {"use": "secondary", "type": "ODS", "value": "org_id"},
-                "active": "true",
-                "type": "Processed Type",
-                "name": "Non-Pharmacy Name",
-                "Address": [
-                    {"street": "456 Second St", "city": "City", "state": "State"}
-                ],
-                "createdDateTime": "formatted_datetime",
-                "createdBy": "Admin",
-                "modifiedBy": "Admin",
-                "modifiedDateTime": "formatted_datetime",
-            },
-        )
+#         # Assert that the result has the expected attributes based on the mock data
+#         self.assertEqual(
+#             result,
+#             {
+#                 "resourceType": "Organization",
+#                 "id": "generated_id",
+#                 "identifier": {"use": "secondary", "type": "ODS", "value": "org_id"},
+#                 "active": "true",
+#                 "type": "Processed Type",
+#                 "name": "Non-Pharmacy Name",
+#                 "Address": [
+#                     {"street": "456 Second St", "city": "City", "state": "State"}
+#                 ],
+#                 "createdDateTime": "formatted_datetime",
+#                 "createdBy": "Admin",
+#                 "modifiedBy": "Admin",
+#                 "modifiedDateTime": "formatted_datetime",
+#             },
+#         )
 
 
 class TestProcessOrganizations(unittest.TestCase):
