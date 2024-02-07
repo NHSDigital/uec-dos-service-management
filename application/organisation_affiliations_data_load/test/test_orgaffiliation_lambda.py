87b26443a70df25f904dcce5c45e631b949fc220
Nachiyappan Lakshmanan <nachiyappan.lakshmanan1@nhs.net>
2024-02-07 22:07:00 +0000

    DR-597 Orgaffili data load

---
+++ application/organisation_affiliations_data_load/test/test_orgaffiliation_lambda.py
@@ -0,0 +1,278 @@
+from application.organisation_affiliations_data_load.orgaffiliation_lambda import (
+    lambda_handler,
+    process_orgaffiliation,
+    write_to_dynamodborgaffili,
+    update_orgaffiliation_org,
+    update_orgaffiliation_partiorg,
+    fetch_orgaffiliation,
+)
+
+import unittest
+from unittest.mock import patch, MagicMock, Mock
+
+
+class TestLambdaHandler(unittest.TestCase):
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.fetch_orgaffiliation"
+    )
+    def test_lambda_handler(self, mock_fetch_orgaffiliation):
+        # Set up mock event and context
+        event = {"some_key": "some_value"}
+        context = MagicMock()
+
+        # Execute the lambda_handler function
+        lambda_handler(event, context)
+
+        # Assertions
+        mock_fetch_orgaffiliation.assert_called_once()
+
+
+class TestUpdateRecords(unittest.TestCase):
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.boto3.resource"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.workspace_orgaffiliation_table_name",
+        "orgaffiliation_table_name",
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.organisations_table_name",
+        "organisations_table_name",
+    )
+    def test_update_orgaffiliation_org(self, mock_resource):
+        # Mock data with existing data in the DynamoDB tables
+        mock_org_table = Mock()
+        mock_orgaffiliation_table = Mock()
+        mock_resource.return_value.Table.side_effect = [
+            mock_org_table,
+            mock_orgaffiliation_table,
+        ]
+
+        mock_org_response = {
+            "Items": [{"identifier": {"value": "123"}, "id": "org_id"}]
+        }
+        mock_orgaffiliation_response = {
+            "Items": [
+                {
+                    "lookup_field_Org": "123",
+                    "id": "organisations_id",
+                    "organization": "",
+                }
+            ]
+        }
+        mock_org_table.scan.return_value = mock_org_response
+        mock_orgaffiliation_table.scan.return_value = mock_orgaffiliation_response
+
+        # Call the function to update records
+        update_orgaffiliation_org()
+
+        # Assert that the update_item method was called with the correct parameters
+        mock_orgaffiliation_table.update_item.assert_called_once_with(
+            Key={"id": "organisations_id"},
+            UpdateExpression="SET organization = :val",
+            ExpressionAttributeValues={":val": "org_id"},
+        )
+
+
+class TestUpdateRecordsParti(unittest.TestCase):
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.boto3.resource"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.workspace_orgaffiliation_table_name",
+        "orgaffiliation_table_name",
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.organisations_table_name",
+        "organisations_table_name",
+    )
+    def update_orgaffiliation_partiorg(self, mock_resource):
+        # Mock data with existing data in the DynamoDB tables
+        mock_org_table = Mock()
+        mock_orgaffiliation_table = Mock()
+        mock_resource.return_value.Table.side_effect = [
+            mock_org_table,
+            mock_orgaffiliation_table,
+        ]
+
+        mock_org_response = {
+            "Items": [{"identifier": {"value": "123"}, "id": "org_id"}]
+        }
+        mock_orgaffiliation_response = {
+            "Items": [
+                {
+                    "lookup_field_parti": "123",
+                    "id": "organisations_id",
+                    "participatingOrganization": "",
+                }
+            ]
+        }
+        mock_org_table.scan.return_value = mock_org_response
+        mock_orgaffiliation_table.scan.return_value = mock_orgaffiliation_response
+
+        # Call the function to update records
+        update_orgaffiliation_partiorg()
+
+        # Assert that the update_item method was called with the correct parameters
+        mock_orgaffiliation_table.update_item.assert_called_once_with(
+            Key={"id": "organisations_id"},
+            UpdateExpression="SET participatingOrganization = :val",
+            ExpressionAttributeValues={":val": "org_id"},
+        )
+
+
+class TestWriteToDynamoDBOrgaffili(unittest.TestCase):
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.boto3.resource"
+    )
+    def test_write_to_dynamodborgaffili(self, mock_boto3_resource):
+        # Mock DynamoDB resource
+        mock_table = MagicMock()
+        mock_boto3_resource.return_value.Table.return_value = mock_table
+
+        # Mock data for testing
+        processed_data = [
+            {"lookup_field": "value1", "other_field": "data1"},
+            {"lookup_field": "value2", "other_field": "data2"},
+        ]
+
+        # Call the function to test
+        write_to_dynamodborgaffili("test_table", processed_data)
+
+        # Assert that put_item was called for each item where data doesn't exist
+        mock_table.put_item.assert_called_with(Item=processed_data[1])
+
+
+class TestProcessorgaffiliation(unittest.TestCase):
+    def test_process_orgaffiliation(self):
+        data = [
+            {
+                "fullUrl": "dummy_url",
+                "resource": {
+                    "resourceType": "OrganizationAffiliation",
+                    "id": "FC999",
+                    "active": "true",
+                    "periodStart": {"start": "2000-01-01"},
+                    "organization": {
+                        "reference": "Organization/FF999",
+                        "identifier": {"system": "dummy_url", "value": "FF999"},
+                    },
+                    "participatingOrganization": {
+                        "reference": "Organization/P0XX",
+                        "identifier": {"system": "dummy_url", "value": "P0XX"},
+                    },
+                    "code": [
+                        {
+                            "coding": [
+                                {
+                                    "system": "dummy_url",
+                                    "code": "XXX",
+                                    "display": "IS OPERATED BY",
+                                }
+                            ]
+                        }
+                    ],
+                },
+            }
+        ]
+        with patch(
+            "application.organisation_affiliations_data_load.orgaffiliation_lambda.uuid"
+        ) as mock_uuid, patch(
+            "application.organisation_affiliations_data_load.orgaffiliation_lambda.datetime"
+        ) as mock_datetime:
+            mock_uuid.uuid4.return_value.int = 1234567890123456
+            mock_datetime.datetime.now.return_value.strftime.return_value = (
+                "01-01-2022 12:00:00"
+            )
+
+            result = process_orgaffiliation(data)
+
+        expected_result = [
+            {
+                "resourceType": "OrganizationAffiliation",
+                "id": "1234567890123456",
+                "identifier": {
+                    "use": "secondary",
+                    "type": "ODS Org. Affiliation id",
+                    "value": "FC999",
+                },
+                "active": "true",
+                "periodStart": "2000-01-01",
+                "organization": "",
+                "participatingOrganization": "",
+                "lookup_field_Org": "FF999",
+                "lookup_field_parti": "P0XX",
+                "code": [
+                    {
+                        "coding": [
+                            {
+                                "system": "dummy_url",
+                                "code": "XXX",
+                                "display": "IS OPERATED BY",
+                            }
+                        ]
+                    }
+                ],
+                "createdDateTime": "01-01-2022 12:00:00",
+                "createdBy": "Admin",
+                "modifiedBy": "Admin",
+                "modifiedDateTime": "01-01-2022 12:00:00",
+            }
+        ]
+
+        self.assertEqual(result, expected_result)
+
+
+class TestFetchOrgaffiliation(unittest.TestCase):
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.common_functions.get_ssm"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.common_functions.get_headers"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.common_functions.read_excel_values"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.common_functions.read_ods_api"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.process_organizations"
+    )
+    @patch(
+        "application.organisation_affiliations_data_load.orgaffiliation_lambda.write_to_dynamodb"
+    )
+    def test_fetch_orgaffiliation(
+        self,
+        mock_write_to_dynamodb,
+        mock_process_organizations,
+        mock_read_ods_api,
+        mock_read_excel_values,
+        mock_get_headers,
+        mock_get_ssm,
+    ):
+        # Mocking necessary functions
+        mock_get_ssm.return_value = "mocked_api_url"
+        mock_get_headers.return_value = {"Authorization": "Bearer token"}
+        mock_read_excel_values.return_value = {"456"}
+        mock_response_data = {
+            "entry": [{"organization": "data1"}, {"organization": "data2"}]
+        }
+        mock_read_ods_api.return_value = mock_response_data
+
+        # Call the function to test
+        fetch_orgaffiliation()
+
+        # Assertions
+        mock_get_ssm.assert_called_once_with("/data/api/lambda/ods/domain")
+        mock_get_headers.assert_called_once()
+        mock_read_excel_values.assert_called_once()
+        mock_read_ods_api.assert_called_with(
+            "mocked_api_url/fhir/OrganizationAffiliation?active=true",
+            {"Authorization": "Bearer token"},
+            "456",
+        )
+        mock_process_organizations.assert_called_once_with(mock_response_data["entry"])
+        mock_write_to_dynamodb.assert_called_once_with(
+            "orgaffiliation", mock_process_organizations.return_value
+        )
---
+++ infrastructure/stacks/support_tools/orgaffiliation_lambda.tf
@@ -0,0 +1,87 @@
+module "orgaffiliation-lambda" {
+  source = "../../modules/lambda"
+
+  function_name = "orgaffiliation_data_load"
+  description   = "To pull orgaffiliation data from ODS and write to DynamoDB Org. Affiliation table"
+  handler       = "orgaffiliation_lambda.lambda_handler"
+  layers = [
+    "arn:aws:lambda:${var.aws_region}:336392948345:layer:AWSSDKPandas-Python39:14",
+    "arn:aws:lambda:${var.aws_region}:${local.account_id}:layer:requests:1"
+  ]
+  timeout = "900"
+
+  environment_variables = {
+  "ODS_CODES_XLSX_FILE" : "ODS_Codes.xlsx",
+  "S3_DATA_BUCKET" : var.sm_datasource_bucket_name,
+  }
+
+  policy_jsons = [
+    <<-EOT
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "ListParameters",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:DescribeParameters"
+                    ],
+                    "Resource": "*"
+                },
+                {
+                    "Sid": "UseParameters",
+                    "Effect": "Allow",
+                    "Action": [
+                        "ssm:GetParameters"
+                    ],
+                    "Resource": [
+                        "arn:aws:ssm:${var.aws_region}:${local.account_id}:parameter/data/api/lambda/client_id",
+                        "arn:aws:ssm:${var.aws_region}:${local.account_id}:parameter/data/api/lambda/client_secret"
+                        "arn:aws:ssm:${var.aws_region}:${local.account_id}:parameter/data/api/lambda/ods/domain"
+                    ]
+                },
+                {
+                    "Sid": "AliasBasedKMS",
+                    "Effect": "Allow",
+                    "Action": [
+                      "kms:Decrypt",
+                      "kms:Encrypt",
+                      "kms:GenerateDataKey*",
+                      "kms:DescribeKey"
+                    ],
+                    "Resource": "arn:aws:kms:${var.aws_region}:${local.account_id}:key/*",
+                    "Condition": {
+                      "StringEquals": {
+                        "kms:ResourceAliases": "alias/aws/ssm"
+                      }
+                    }
+                },
+                {
+                    "Sid": "DynamodbTable",
+                    "Effect": "Allow",
+                    "Action": [
+                        "dynamodb:PutItem",
+                        "dynamodb:GetItem",
+                        "dynamodb:Scan",
+                        "dynamodb:Query",
+                        "dynamodb:UpdateItem"
+                    ],
+                    "Resource": [
+                        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/organisation_affiliations${local.workspace_suffix}",
+                        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/organisations"
+                    ]
+                },
+                {
+                    "Sid": "ListObjectsInBucket",
+                    "Effect": "Allow",
+                    "Action": [
+                      "s3:ListBucket",
+                      "s3:GetObject"
+                    ],
+                    "Resource": ["*"]
+                }
+            ]
+        }
+        EOT
+  ]
+}
