cbe59ab79a2686c93eb2aed199f986663b317774
pira Maheswaran <piravinth.maheswaran1@nhs.net>
2024-03-28 09:53:25 +0000

    DR-764 Update ports for sg

--- infrastructure/stacks/application/security_group.tf
+++ infrastructure/stacks/application/security_group.tf
@@ -12,7 +12,7 @@ resource "aws_vpc_security_group_egress_rule" "lambda_egress" {
   security_group_id = aws_security_group.application_lambda_sg.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
+  from_port   = "0"
   ip_protocol = "-1"
-  to_port     = 0
+  to_port     = "0"
 }
