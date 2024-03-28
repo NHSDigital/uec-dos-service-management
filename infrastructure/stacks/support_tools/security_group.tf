e773315627adf4816cd45da9a266eb2a60b8327e
Pira-nhs <138581534+Pira-nhs@users.noreply.github.com>
2024-03-28 14:59:59 +0000

    Task/dr 764 add lambdas tovpc (#117)
    * DR-764 Add lambdas to vpc
    
    * DR-764 Add lambdas to vpc
    
    * DR-764 Add supporttools to vpc
    
    * DR-764 Tweak changes to supporttools
    
    * DR-764 Tweak changes to supporttools
    
    * DR-764 Create sg for supporttools
    
    * DR-764 Create sg for supporttools
    
    * DR-764 Create sg for supporttools
    
    * DR-764 Modify common vars
    
    * DR-764 Remove common vars
    
    * DR-764 Update ports for sg
    
    * DR-764 Remove ports for sg
    
    * DR-764 Bugfix sg duplication
    
    * DR-764 Bugfix sg duplication

--- infrastructure/stacks/support_tools/security_group.tf
+++ infrastructure/stacks/support_tools/security_group.tf
@@ -1,10 +1,14 @@
 resource "aws_security_group" "support_tools_lambda_sg" {
-  name        = "support-tools-lambda-sg"
+  name_prefix = "support-tools-lambda-sg"
   description = "Security group for support tools lambdas"
   vpc_id      = data.aws_vpc.main.id
 
   tags = {
     Name = "support-tools-lambda-sg"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
