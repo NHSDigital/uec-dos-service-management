1b1ea0258b3b9e519c2ca44f85d002b1795c9e8f
Jonathan Pearce <j.pearce1@nhs.net>
2024-04-04 16:09:13 +0100

    DR-764 Workspace lambda sgs

--- infrastructure/modules/security-group/main.tf
+++ infrastructure/modules/security-group/main.tf
@@ -4,9 +4,8 @@ resource "aws_security_group" "security_group" {
   vpc_id      = data.aws_vpc.vpc.id
 }
 
-# TODO Check that this is an acceptable policy for all SGs
 resource "aws_vpc_security_group_egress_rule" "lambda_egress" {
-  count = var.apply_default_egress
+  count = (var.apply_default_egress == true ? 1 : 0)
 
   security_group_id = aws_security_group.security_group.id
   cidr_ipv4         = "0.0.0.0/0"
