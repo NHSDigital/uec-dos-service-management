7b04066a5afe561a16dab57734f1a4f562010779
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-22 11:42:36 +0100

    DR-630 Upgrade python version (#123)
    * DR-630 Upgrade python version
    
    * DR-630 Align with new names
    
    * DR-630 Uplift v014 of dos management
    
    * DR-630 Move up hashicorp provider vers
    
    * DR-630 From status ok to 200

--- application/organisations-data-manager/test/test_app.py
+++ application/organisations-data-manager/test/test_app.py
@@ -75,7 +75,7 @@ def test_get_record_by_id():
     mock_context = SimpleNamespace(**lambda_context_data)
     mock_load = load_sample_event_from_file("mock_proxy_get_event.json")
     response = app.lambda_handler(mock_load, mock_context)
-    assert str(response["statusCode"]) == "HTTPStatus.OK"
+    assert str(response["statusCode"]) == "200"
     response_body = json.loads(response["body"])
     assert response_body["Item"]["name"] == mock_name
 
@@ -88,7 +88,7 @@ def test_put_record():
     mock_context = SimpleNamespace(**lambda_context_data)
     mock_load = load_sample_event_from_file("mock_proxy_put_event.json")
     response = app.lambda_handler(mock_load, mock_context)
-    assert str(response["statusCode"]) == "HTTPStatus.OK"
+    assert str(response["statusCode"]) == "200"
     organisations_record = table.get_item(Key={"id": mock_id})
     assert organisations_record["Item"]["name"] == "Nhs PUT Integrated Care Board"
 
@@ -101,7 +101,7 @@ def test_post_record():
     mock_context = SimpleNamespace(**lambda_context_data)
     mock_load = load_sample_event_from_file("mock_proxy_post_event.json")
     response = app.lambda_handler(mock_load, mock_context)
-    assert str(response["statusCode"]) == "HTTPStatus.OK"
+    assert str(response["statusCode"]) == "200"
     organisations_record = table.get_item(Key={"id": mock_id})
     assert organisations_record["Item"]["name"] == "Nhs POST Integrated Care Board"
 
