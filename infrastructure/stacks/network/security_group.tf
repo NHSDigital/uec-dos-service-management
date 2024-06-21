bc1e4e7d06b9cd554d4c9c0026c8876b86aa3656
piravinth <piravinth.maheswaran1@nhs.net>
2024-06-21 11:15:34 +0100

    DR-930 Modify security group

--- infrastructure/stacks/network/security_group.tf
+++ infrastructure/stacks/network/security_group.tf
@@ -1,6 +1,10 @@
 module "application_lambda_security_group" {
   source = "../../modules/security-group"
 
+  depends_on = [
+      aws_vpc. main
+  ]
+
   vpc_name             = "${var.project}-${var.vpc_name}-${var.environment}"
   name                 = var.application_lambda_security_group_name
   description          = "Security group for application lambda"
@@ -10,10 +14,13 @@ module "application_lambda_security_group" {
 module "data_migration_lambda_security_group" {
   source = "../../modules/security-group"
 
+  depends_on = [
+      aws_vpc. main
+  ]
+
   vpc_name             = "${var.project}-${var.vpc_name}-${var.environment}"
   name                 = var.data_migration_lambda_security_group_name
   description          = "Security group for data migration lambdas"
   apply_default_egress = true
 }
-
 
