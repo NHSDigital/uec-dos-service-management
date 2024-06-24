1cb5b15df1581132d3b62602ca384226bc198dd5
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2024-06-24 15:12:54 +0100

    DR-1001 Sorting out the tf

--- infrastructure/modules/api-gateway-rest-api/main.tf
+++ infrastructure/modules/api-gateway-rest-api/main.tf
@@ -1,6 +1,5 @@
 resource "aws_api_gateway_rest_api" "rest_api" {
-  name = "${var.rest_api_name}${local.workspace_suffix}"
-  xray_tracing_enabled = true
+  name                 = "${var.rest_api_name}${local.workspace_suffix}"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
