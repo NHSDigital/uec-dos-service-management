d5f299223456235f71c8af00a428fc5fc0b2fd34
Tim Rickwood <tim.rickwood@nhs.net>
2024-08-06 16:21:36 +0100

    DR-10131 Pre review tidy up

--- scripts/create-lambda-layer.sh
+++ scripts/create-lambda-layer.sh
@@ -4,22 +4,6 @@
 set -e
 # Before running this script:
 #  - Login to an appropriate AWS account as appropriate user from commamnd-line
-#  - Export the following variables prior to calling this script
-#  -
-
-# check required exports have been done
-EXPORTS_SET=0
-# # Check key variables have been exported - see above
-# if [ -z "$USER_SHORTCODE" ] ; then
-#   echo  USER_SHORTCODE not set
-#   EXPORTS_SET=1
-# fi
-
-if [ $EXPORTS_SET = 1 ] ; then
-  echo One or more exports not set
-  exit 1
-fi
-
 
 echo Run pip install of requests package
 mkdir python
