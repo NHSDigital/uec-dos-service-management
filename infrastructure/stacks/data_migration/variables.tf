35e6cc758cad00c7cee15d664ed5d03a0d754fb3
piravinth <piravinth.maheswaran1@nhs.net>
2024-08-08 15:30:27 +0100

    DR-1013 Implement prod workflow

--- infrastructure/stacks/data_migration/variables.tf
+++ infrastructure/stacks/data_migration/variables.tf
@@ -1,5 +1,3 @@
 variable "sm_datasource_bucket_name" {
   description = "Name of s3 bucket that holds sm excel data sheet"
-  type        = string
-  default     = "nhse-${var.project}-${var.environment}-databucket"
 }
