50f32427c1df525e99f424a03d65e6dc07fceeb2
piravinth <piravinth.maheswaran1@nhs.net>
2024-08-14 16:47:04 +0100

    DR-1013 Testing tag usage

--- infrastructure/stacks/domain-test/s3_bucket.tf
+++ infrastructure/stacks/domain-test/s3_bucket.tf
@@ -3,9 +3,3 @@ module "s3_bucket" {
   bucket_name   = "${var.s3_bucket_name}-${var.environment}${local.workspace_suffix}"
   force_destroy = var.force_destroy
 }
-
-module "s3_tag_test_bucket" {
-  source        = "../../modules/s3"
-  bucket_name   = "testing-tag-pira"
-  force_destroy = var.force_destroy
-}
